9f959dca5868aa4d609aca7cc4bffc1d92725cb1
Hugo Doueil <46538211+Ormidales@users.noreply.github.com>
2022-07-08 21:47:28 +0200

    Add files via upload

---
+++ scolaire/ppe2/ppe2v1/lang/es/code/php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- INFORMATIONS DU SITE -->
+	<title><?php echo "salutation" ?></title>
+	<meta charset="utf-8">
+	<meta lang="fr">
+	<meta name="author" content="Hugo, Bertrand, Melvin">
+	<meta name="description" content="Un site pour présenter le BTS SIO SLAM du lycée Pardailhan de Auch">
+	<meta name="keywords" content="bts, sio, BTS, SIO, SLAM, slam, hugo, bertrand, melvin, webiste, html, css, php, javascript">
+
+	<!-- LINKS DU SITE -->
+	<link rel="stylesheet" type="text/css" href="../css/index.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&family=Raleway&family=Ubuntu&display=swap" rel="stylesheet">
+</head>
+<body>
+	<?php
+		echo "<p>bonjour monde</p>";
+	?>
+</body>
+</html>
