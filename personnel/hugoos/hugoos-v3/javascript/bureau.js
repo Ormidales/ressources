88b9ceb0e31baa2f7ef771bc92120a381ce8f6b9
Hugo Doueil <46538211+Ormidales@users.noreply.github.com>
2022-07-08 21:12:58 +0200

    Add files via upload

---
+++ personnel/hugoos/hugoos-v3/javascript/bureau.js
@@ -0,0 +1,93 @@
+/* APPLICATION WELCOME */
+function menuAppWelcome(){
+    document.getElementById("app-welcome").style.display = "block";
+    document.getElementById("app-welcome").style.animationName = "appIn";
+    document.getElementById("app-welcome").style.animationDuration = "0.4s";
+}
+function appWelcomeClose(){
+    document.getElementById("app-welcome").style.display = "none";
+}
+
+
+
+/* APPLICATION ACCUEIL */
+function menuAppAccueil(){
+    document.getElementById("app-accueil").style.display = "block";
+    document.getElementById("app-accueil").style.animationName = "appIn";
+    document.getElementById("app-accueil").style.animationDuration = "0.4s";
+}
+function appAccueilClose(){
+    document.getElementById("app-accueil").style.display = "none";
+}
+
+
+
+/* APPLICATION PROJETS */
+function menuAppProjets(){
+    document.getElementById("app-projets").style.display = "block";
+    document.getElementById("app-projets").style.animationName = "appIn";
+    document.getElementById("app-projets").style.animationDuration = "0.4s";
+}
+function appProjetsClose(){
+    document.getElementById("app-projets").style.display = "none";
+}
+
+
+
+/* APPLICATION STAGES */
+function menuAppStages(){
+    document.getElementById("app-stages").style.display = "block";
+    document.getElementById("app-stages").style.animationName = "appIn";
+    document.getElementById("app-stages").style.animationDuration = "0.4s";
+}
+function appStagesClose(){
+    document.getElementById("app-stages").style.display = "none";
+}
+
+
+
+/* APPLICATION TACHES */
+function menuAppTaches(){
+    document.getElementById("app-taches").style.display = "block";
+    document.getElementById("app-taches").style.animationName = "appIn";
+    document.getElementById("app-taches").style.animationDuration = "0.4s";
+}
+function appTachesClose(){
+    document.getElementById("app-taches").style.display = "none";
+}
+
+
+
+/* APPLICATION CATALOGUE APP */
+function menuAppCatalog(){
+    document.getElementById("app-catalog").style.display = "block";
+    document.getElementById("app-catalog").style.animationName = "appIn";
+    document.getElementById("app-catalog").style.animationDuration = "0.4s";
+}
+function appCatalogClose(){
+    document.getElementById("app-catalog").style.display = "none";
+}
+
+
+
+/* APPLICATION CV */
+function appCV(){
+    document.getElementById("app-cv").style.display = "block";
+    document.getElementById("app-cv").style.animationName = "appIn";
+    document.getElementById("app-cv").style.animationDuration = "0.4s";
+}
+function appCVClose(){
+    document.getElementById("app-cv").style.display = "none";
+}
+
+
+
+/* MENU RETROCOMPATIBILITE */
+function menuRetrocomp(){
+    document.getElementById("menu-retrocomp").style.display = "block";
+    document.getElementById("menu-retrocomp").style.animationName = "menuRetrocompIn";
+    document.getElementById("menu-retrocomp").style.animationDuration = "0.4s";
+}
+function menuRetrocompClose(){
+    document.getElementById("menu-retrocomp").style.display = "none";
+}
