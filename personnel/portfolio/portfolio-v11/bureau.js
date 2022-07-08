3b84a09c47b1c8f7bb256187c9f21cafbc0159e9
Hugo Doueil <46538211+Ormidales@users.noreply.github.com>
2022-07-08 20:34:29 +0200

    Add files via upload

---
+++ personnel/portfolio/portfolio-v11/bureau.js
@@ -0,0 +1,62 @@
+function boutonAppParametreClickedIn(){
+    document.getElementById("application-parametre").style.display = "block";
+    document.getElementById("application-parametre").style.animationName = "applicationIn";
+    document.getElementById("application-parametre").style.animationDuration = "0.5s";
+}
+function boutonAppParametreClickedOut(){
+    document.getElementById("application-parametre").style.display = "none";
+}
+
+
+
+function boutonAppCatalogueClickedIn(){
+    document.getElementById("application-catalogue").style.display = "block";
+    document.getElementById("application-catalogue").style.animationName = "applicationIn";
+    document.getElementById("application-catalogue").style.animationDuration = "0.5s";
+}
+function boutonAppCatalogueClickedOut(){
+    document.getElementById("application-catalogue").style.display = "none";
+}
+
+
+
+function boutonAppChangementthemeClickedIn(){
+    document.getElementById("application-changementtheme").style.display = "block";
+    document.getElementById("application-changementtheme").style.animationName = "applicationIn";
+    document.getElementById("application-changementtheme").style.animationDuration = "0.5s";
+}
+function boutonAppChangementthemeClickedOut(){
+    document.getElementById("application-changementtheme").style.display = "none";
+}
+
+
+
+function boutonMenuRetrocompClickedIn(){
+    document.getElementById("menu-retrocomp").style.display = "flex";
+    document.getElementById("menu-retrocomp").style.animationName = "menuIn";
+    document.getElementById("menu-retrocomp").style.animationDuration = "0.5s";
+}
+function boutonMenuRetrocompClickedOut(){
+    document.getElementById("menu-retrocomp").style.display = "none";
+    document.getElementById("sub-retrocomp").style.display = "none";
+}
+function boutonSubRetrocompClickedIn(){
+    document.getElementById("sub-retrocomp").style.display = "flex";
+    document.getElementById("sub-retrocomp").style.animationName = "menuIn";
+    document.getElementById("sub-retrocomp").style.animationDuration = "0.5s";
+}
+
+
+
+function boutonChangementThemeTropical(){
+    document.getElementById("bureau").style.backgroundImage = "url('img/background9.png')";
+}
+function boutonChangementThemeRoseetbleu(){
+    document.getElementById("bureau").style.backgroundImage = "url('img/background2.png')";
+}
+function boutonChangementThemeRoseetjaune(){
+    document.getElementById("bureau").style.backgroundImage = "url('img/background14.png')";
+}
+function boutonChangementThemeLicorne(){
+    document.getElementById("bureau").style.backgroundImage = "url('img/licorne.jpg')";
+}
