6960bceef5b7ae42c5d342443cb120804ae3a86e
Hugo Doueil <46538211+Ormidales@users.noreply.github.com>
2022-07-08 21:33:21 +0200

    Add files via upload

---
+++ scolaire/ppe3/ppe3v2/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" lang="fr">
+
+		<!-- Primary Meta Tags -->
+		<title>PPE3 V2 / FaSiLa | École d'arts</title>
+		<meta name="title" content="PPE3 V2 / FaSiLa | École d'arts">
+		<meta name="description" content="PPE3 V2 / FaSiLa | École d'arts - Prenez rendez vous pour un cours de danse - SA VA SWINGER !!!">
+
+		<!-- Open Graph / Facebook -->
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://metatags.io/">
+		<meta property="og:title" content="PPE3 V2 / FaSiLa | École d'arts">
+		<meta property="og:description" content="PPE3 V2 / FaSiLa | École d'arts - Prenez rendez vous pour un cours de danse - SA VA SWINGER !!!">
+		<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+		<!-- Twitter -->
+		<meta property="twitter:card" content="summary_large_image">
+		<meta property="twitter:url" content="https://metatags.io/">
+		<meta property="twitter:title" content="PPE3 V2 / FaSiLa | École d'arts">
+		<meta property="twitter:description" content="PPE3 V2 / FaSiLa | École d'arts - Prenez rendez vous pour un cours de danse - SA VA SWINGER !!!">
+		<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+		<!-- ua -->
+		<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+
+		<meta name='mobile-web-app-capable' content='yes'>
+		<meta name='apple-mobile-web-app-capable' content='yes'>
+
+		<meta name='theme-color' content='#ff00ff'>
+
+		<!-- viewport -->
+		<meta name='viewport' content='width=device-width'>
+
+		<!-- UIkit CSS -->
+		<link rel="stylesheet" href="css/uikit.css" />
+
+		<!-- UIkit JS -->
+		<script src="js/uikit.min.js"></script>
+		<script src="js/uikit-icons.min.js"></script>
+	</head>
+	<body>
+		<div class="all-content" id="all-content">
+			
+			<!-- HEADER -->
+			<div uk-sticky="media: 960" class="uk-navbar-container tm-navbar-container uk-sticky uk-sticky-fixed" style="position: fixed; top: 0px; width: 1214px;">
+				<div class="uk-container uk-container-expand uk-background-primary">
+					<nav class="uk-navbar">
+						<div class="uk-navbar-left">
+							<a href="#" class="uk-navbar-item uk-logo" style="color: white;">
+								<svg width="28" height="34" viewBox="0 0 28 34" xmlns="http://www.w3.org/2000/svg" class="uk-margin-small-right uk-svg">
+									<polygon fill="#fff" points="19.1,4.1 13.75,1 8.17,4.45 13.6,7.44 "></polygon>
+									<path fill="#fff" d="M21.67,5.43l-5.53,3.34l6.26,3.63v9.52l-8.44,4.76L5.6,21.93v-7.38L0,11.7v13.51l13.75,8.08L28,25.21V9.07 L21.67,5.43z"></path>
+								</svg>
+								FaSiLa | École d'arts
+							</a>
+						</div>
+						<div class="uk-navbar-right uk-margin-small-right" style="color: white;">
+							<ul class="uk-navbar-nav uk-visible@m uk-light">
+								<li>
+									<a href="#introduction" class="uk-active uk-button-text">Accueil</a>
+								</li>
+								<li>
+									<a href="#" class="uk-button-text">Inscription</a>
+								</li>
+								<li>
+									<a href="#" class="uk-button-text">Tarifs</a>
+								</li>
+							</ul>
+							<a uk-navbar-toggle-icon href="#offcanvas" uk-toggle class="uk-navbar-toggle uk-hidden@m uk-icon uk-navbar-toggle-icon" aria-expanded="false" style="color: white;">
+								<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+									<rect y="9" width="20" height="2"></rect>
+									<rect y="3" width="20" height="2"></rect>
+									<rect y="15" width="20" height="2"></rect>
+								</svg>
+							</a>
+						</div>
+					</nav>
+				</div>
+			</div>
+
+			<!-- HEADER NAV SI MOBILE -->
+			<div id="offcanvas" uk-offcanvas="mode: push; overlay: true" class="uk-offcanvas">
+				<div class="uk-offcanvas-bar">
+					<div class="uk-panel">
+						<ul class="uk-nav uk-nav-default tm-nav">
+							<li class="uk-nav-header">General</li>
+							<li>
+								<a href="#introduction" class="uk-button-text">Accueil</a>
+							</li>
+							<li>
+								<a href="#" class="uk-button-text">Inscription</a>
+							</li>
+							<li>
+								<a href="#" class="uk-button-text">Tarifs</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="uk-section uk-section-default" style="background-color: #252525;">
+				<div class="uk-container uk-container-small uk-position-relative">
+					<div>
+						<div class="uk-light">
+							<h1 id="introduction" class="uk-h1 tm-heading-fragment"><a href="#introduction">Accueil</a></h1>
+							<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</body>
+</html>
